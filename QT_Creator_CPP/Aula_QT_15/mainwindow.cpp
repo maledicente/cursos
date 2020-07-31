f71c3395683d7228134c28b437f474c87f0da58a
maledicente <luizpaulonievola@ymail.com>
2020-07-31 18:08:05 -0300

    add

--- QT_Creator_CPP/Aula_QT_15/mainwindow.cpp
+++ QT_Creator_CPP/Aula_QT_15/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QMessageBox"
+
+QString msg="";
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +16,21 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    bool cb1,cb2,cb3;
+    cb1=ui->checkBox->isChecked();
+    cb2=ui->checkBox_2->isChecked();
+    cb3=ui->checkBox_3->isChecked();
+
+    msg="";
+    if(cb1){
+        msg+="CB1 marcado,";
+    }if(cb2){
+        msg+=" CB2 marcado,";
+    }if(cb3){
+        msg+=" CB3 marcado";
+    }
+    QMessageBox::information(this,"Checkboxes ",msg);
+}
