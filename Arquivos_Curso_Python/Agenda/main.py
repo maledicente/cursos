441b947153422f2ce33d9525950204d8badde108
Luiz Paulo <luizpaulonievola@ymail.com>
2020-09-23 16:20:02 -0300

    Agenda fim

--- Arquivos_Curso_Python/Agenda/main.py
+++ Arquivos_Curso_Python/Agenda/main.py
@@ -37,7 +37,7 @@ def menuPrincipal():
 	print("1 - Inserir Novo Registro")
 	print("2 - Deletar Registro")
 	print("3 - Atualizar Registro")
-	print("4 - Consultar Registro por ID")
+	print("4 - Consultar Registros")
 	print("5 - Consultar Registro por Nome")
 	print("6 - Sair")
 
@@ -47,20 +47,64 @@ def menuInserir():
 	vtelefone=input("Digite o telefone: ")
 	vemail=input("Digite o email: ")
 	vsql="INSERT INTO tb_contatos (T_NOMECONTATO, T_telefonecontato, t_emailcontato) VALUES ('"+vnome+"','"+vtelefone+"','"+vemail+"')"
+	vcon=ConexaoBanco()
 	query(vcon,vsql)
+	vcon.close()
 
 
 def menuDeletar():
-	print()
+	os.system("clear")
+	vid=input("Digite o ID do registro a ser deletado: ")
+	vsql="DELETE FROM tb_contatos WHERE N_IDCONTATO="+vid
+	query(vcon,vsql)
 
 def menuAtualizar():
-	print()
+	os.system("clear")
+	vid=input("Digite o ID do registro a ser alterado: ")
+	r=consultar(vcon,"SELECT *FROM tb_contatos WHERE N_IDCONTATO="+vid)
+	rnome=r[0][1]
+	rtelefone=r[0][2]
+	remail=r[0][3]
+	vnome=input("Digite o nome: ")
+	vtelefone=input("Digite o telefone: ")
+	vemail=input("Digite o email: ")
+	if(len(vnome)==0):
+		vnome=rnome
+	if(len(vtelefone)==0):
+		vtelefone=rtelefone
+	if(len(vemail)==0):
+		vemail=remail
+	vsql="UPDATE tb_contatos SET _NOMECONTATO='"+vnome+"', T_telefonecontato='"+vtelefone+"', t_emailcontato='"+vemail+"' WHERE N_IDCONTATO="+vid
+	query(vcon,vsql)
+
 
 def menuConsultarId():
-	print()
+	vsql="SELECT * FROM tb_contatos"
+	res=consultar(vcon,vsql)
+	vlim=10
+	vcont=0
+	for r in res:
+		print("ID:{0:_<3} Nome:{1:_<30} Telefone:{2:_<14} Email:{3:_<30}".format(r[0],r[1],r[2],r[3]))
+		vcont+=1;
+		if(vcont>=vlim):
+			vcont=0
+			#os.system("clear")
+	print("Fim da lista")
+
 
 def menuConsultarNomes():
-	print()
+	vnome=input("Digite o nome: ")
+	vsql="SELECT * FROM tb_contatos WHERE T_NOMECONTATO LIKE '%"+vnome+"%'"
+	res=consultar(vcon,vsql)
+	vlim=10
+	vcont=0
+	for r in res:
+		print("ID:{0:_<3} Nome:{1:_<30} Telefone:{2:_<14} Email:{3:_<30}".format(r[0],r[1],r[2],r[3]))
+		vcont+=1;
+		if(vcont>=vlim):
+			vcont=0
+			#os.system("clear")
+	print("Fim da lista")
 
 opc=0
 while opc !=6:
@@ -75,7 +119,7 @@ while opc !=6:
 	elif opc==4:
 		menuConsultarId()
 	elif opc==5:
-		menuConsultarNome()
+		menuConsultarNomes()
 	elif opc==6:
 		os.system("clear")
 		print("Programa finalizado")
