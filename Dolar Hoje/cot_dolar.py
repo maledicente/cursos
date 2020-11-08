5a1fd59ef38de233bc9bcb5a5352bec6830e5fb7
Luiz Paulo <luizpaulonievola@ymail.com>
2020-11-08 18:44:06 -0300

    Add maps choropleth

--- Dolar Hoje/cot_dolar.py
+++ Dolar Hoje/cot_dolar.py
@@ -1,3 +1,14 @@
+import requests
+import json
+
+req = requests.get('https://economia.awesomeapi.com.br/all/USD-BRL').json()
+
+print('#### Cotação do Dolar ####')
+print ('Moeda: ' + req['USD']['name'])
+print ('Data: ' + req['USD']['create_date'])
+print('Valor atual: R$' + req['USD']['bid'])
+
+"""
 #Importando bibliotecas. 
 #import numpy as np
 #import pandas as pd
@@ -14,3 +25,6 @@ print('#### Cotação do Dolar ####')
 print ('Moeda: ' + cotacao['USD']['name'])
 print ('Data: ' + cotacao['USD']['create_date'])
 print('Valor atual: R$' + cotacao['USD']['bid'])
+"""
+
+
